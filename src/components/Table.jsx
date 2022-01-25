d4a5064050a772b65be9e1bb11a9824f71c8f403
Eddiebee <edbeeotudor@gmail.com>
2022-01-25 10:46:38 +0100

    Build up sorting in ascending and descending order

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,11 @@
 import TableBody from "./TableBody";
 import TableHead from "./TableHead";
+import { useState } from "react";
 
 const Table = ({ headings, data, setData }) => {
+  // set table sort order to ascending
+  const [isAscending, setIsAscending] = useState(true);
+
   const handleSort = (sortBy) => {
     const sortData = [...data];
     let sortby = sortBy.toLowerCase();
@@ -10,8 +14,13 @@ const Table = ({ headings, data, setData }) => {
         let sortByA = a[sortby].name.toUpperCase(),
           sortByB = b[sortby].name.toUpperCase();
 
-        if (sortByA < sortByB) return -1;
-        if (sortByA > sortByB) return 1;
+        if (isAscending) {
+          if (sortByA < sortByB) return -1;
+          if (sortByA > sortByB) return 1;
+          return 0;
+        }
+        if (sortByA < sortByB) return 1;
+        if (sortByA > sortByB) return -1;
         return 0;
       });
     } else if (sortby === "title") {
@@ -19,15 +28,24 @@ const Table = ({ headings, data, setData }) => {
         let sortByA = a[sortby].toUpperCase(),
           sortByB = b[sortby].toUpperCase();
 
-        if (sortByA < sortByB) return -1;
-        if (sortByA > sortByB) return 1;
+        if (isAscending) {
+          if (sortByA < sortByB) return -1;
+          if (sortByA > sortByB) return 1;
+          return 0;
+        }
+        if (sortByA < sortByB) return 1;
+        if (sortByA > sortByB) return -1;
         return 0;
       });
     }
     sortData.sort((a, b) => {
+      if (isAscending) {
+        return b[sortby] - a[sortby];
+      }
       return a[sortby] - b[sortby];
     });
     setData(sortData);
+    setIsAscending(!isAscending);
   };
 
   return (
