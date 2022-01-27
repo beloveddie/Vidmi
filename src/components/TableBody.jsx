3596481fb68d301ede957b2345f267bfd854e791
Eddiebee <edbeeotudor@gmail.com>
2022-01-27 16:09:38 +0100

    Modify TableBody component

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -3,30 +3,31 @@ import { FaThumbsUp, FaRegThumbsUp } from "react-icons/fa";
 const TableBody = ({ data, handleDelete, handleLike }) => {
   return (
     <tbody>
-      {data.map((datum) => {
-        const { _id, title, genre, stock, rate, liked } = datum;
+      {data &&
+        data.map((datum) => {
+          const { _id, title, genre, stock, rate, liked } = datum;
 
-        return (
-          <tr key={_id}>
-            <td>{title}</td>
-            <td>{genre.name}</td>
-            <td>{stock}</td>
-            <td>{rate}</td>
-            <td onClick={() => handleLike(datum)} className="like">
-              {liked ? <FaThumbsUp /> : <FaRegThumbsUp />}
-            </td>
-            <td>
-              <button
-                className="btn btn-sm btn-danger"
-                id="delete"
-                onClick={() => handleDelete(_id)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-      })}
+          return (
+            <tr key={_id}>
+              <td>{title}</td>
+              <td>{genre.name}</td>
+              <td>{stock}</td>
+              <td>{rate}</td>
+              <td onClick={() => handleLike(datum)} className="like">
+                {liked ? <FaThumbsUp /> : <FaRegThumbsUp />}
+              </td>
+              <td>
+                <button
+                  className="btn btn-sm btn-danger"
+                  id="delete"
+                  onClick={() => handleDelete(_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
     </tbody>
   );
 };
